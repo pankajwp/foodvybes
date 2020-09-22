5fc2aa17129898d321089bc556cf790fb051cb95
Pankaj Kumar <pankaj151290mech@gmail.com>
2020-09-22 16:20:51 +0530

    Update dev.js
    removed key

--- config/dev.js
+++ config/dev.js
@@ -1,5 +1,5 @@
 // dev js dont commit this
 module.exports = {
 	mUri:
-		"mongodb+srv://emaily-prod:pcfl9BGlbhWaMP2E@cluster0-b3mzq.mongodb.net/FoodVybe?retryWrites=true",
+		"mongodb+srv://<username>:<password>@<cluster>.mongodb.net/FoodVybe?retryWrites=true",
 };
